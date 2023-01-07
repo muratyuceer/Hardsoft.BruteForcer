fd43733566b3a5608da519aeab2e8a4640d3951d
muratyuceer <murat_yuceer@hotmail.com>
2023-01-07 17:31:44 +0300

    init

---
+++ test/Hardsoft.BruteForcer.UnitTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Hardsoft.BruteForcer.UnitTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
